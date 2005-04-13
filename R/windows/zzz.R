973370b17c81fb5cc8edb220d3fcdbe9b14d63e6
unknown <colin@colinsmith.org>
2005-04-13 04:21:54 +0000

    Moved Windows netcdf.dll out of inst/libs into inst/netcdfdll
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@11805 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/windows/zzz.R
+++ R/windows/zzz.R
@@ -1,9 +1,13 @@
 .First.lib <- function(libname, pkgname) {
-    suppressWarnings(library.dynam("netcdf", pkgname, libname))
+    dllpath <- file.path(.find.package("xcms"), "netcdfdll", "netcdf.dll")
+    suppressWarnings(dyn.load(dllpath))
     library.dynam("xcms", pkgname, libname)    
 }
 
 .Last.lib <- function(libpath) {
-    library.dynam.unload("xcms", libpath)
-    library.dynam.unload("netcdf", libpath)
+    if (is.loaded("NetCDFOpen"))
+        library.dynam.unload("xcms", libpath)
+    dllpath <- file.path(.find.package("xcms"), "netcdfdll", "netcdf.dll")
+    if (is.loaded("nc_strerror"))
+        dyn.unload(dllpath)
 }
