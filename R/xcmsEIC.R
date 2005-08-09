4c6550352bb2482643187a9daf23c6b8908cbb2e
Colin Smith <colin@colinsmith.org>
2005-08-09 17:58:16 +0000

    Added xcmsEIC class and fixed several bugs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@13513 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/xcmsEIC.R
@@ -0,0 +1,164 @@
+setClass("xcmsEIC", representation(eic = "list", mzrange = "matrix",
+                                   rtrange = "matrix", rt = "character", 
+                                   groupnames = "character"),
+         prototype(eic = list(), mzrange = matrix(nrow = 0, ncol = 0), 
+                   rtrange = matrix(nrow = 0, ncol = 0), 
+                   rt = character(0), groupnames = character(0)))
+
+if ( !isGeneric("show") )
+    setGeneric("show", function(object) standardGeneric("show"))
+
+setMethod("show", "xcmsEIC", function(object) {
+
+    cat("An \"xcmsEIC\" object with", length(object@eic), "samples\n\n")
+    
+    cat("Time range: ", paste(round(range(object@rtrange), 1), collapse = "-"), 
+        " seconds (", paste(round(range(object@rtrange)/60, 1), collapse = "-"), 
+        " minutes)\n", sep = "")
+    cat("Mass range:", paste(round(range(object@mzrange, na.rm = TRUE), 4), collapse = "-"), 
+        "m/z\n")
+    cat("EICs per sample:", nrow(object@mzrange), "\n")
+    cat("Retention times:", object@rt, "\n")
+    cat("xcmsSet group names: ")
+    if (length(object@groupnames))
+        cat("present")
+    else
+        cat("absent")
+    cat("\n\n")
+    
+    
+    memsize <- object.size(object)
+    cat("Memory usage:", signif(memsize/2^20, 3), "MB\n")
+})
+
+if ( !isGeneric("groupnames") )
+    setGeneric("groupnames", function(object, ...) standardGeneric("groupnames"))
+
+setMethod("groupnames", "xcmsEIC", function(object) {
+
+    object@groupnames
+})
+
+if ( !isGeneric("sampnames") )
+    setGeneric("sampnames", function(object) standardGeneric("sampnames"))
+
+setMethod("sampnames", "xcmsEIC", function(object) {
+
+    names(object@eic)
+})
+
+if ( !isGeneric("mzrange") )
+    setGeneric("mzrange", function(object) standardGeneric("mzrange"))
+
+setMethod("mzrange", "xcmsEIC", function(object) {
+
+    object@mzrange
+})
+
+if ( !isGeneric("rtrange") )
+    setGeneric("rtrange", function(object) standardGeneric("rtrange"))
+
+setMethod("rtrange", "xcmsEIC", function(object) {
+
+    object@rtrange
+})
+
+if ( !isGeneric("plot") )
+    setGeneric("plot", function(x, y, ...) standardGeneric("plot"))
+
+setMethod("plot", "xcmsEIC", function(x, y, groupidx = groupnames(x),
+                                      sampleidx = sampnames(x), 
+                                      rtrange = x@rtrange, 
+                                      col = rep(1, length(sampleidx)), 
+                                      legtext = NULL, peakint = TRUE, 
+                                      sleep = 0) {
+
+    object <- x
+    
+    if (is.numeric(groupidx))
+       groupidx <- object@groupnames[groupidx]
+    grpidx <- match(groupidx, object@groupnames)
+    
+    if (is.numeric(sampleidx))
+       sampleidx <- names(object@eic)[sampleidx]
+    sampidx <- match(sampleidx, names(object@eic))
+    
+    if (length(rtrange) == 1)
+        rtrange <- retexp(object@rtrange, rtrange)
+    else if (length(rtrange) < length(object@rtrange)) {
+        tmprtrange <- matrix(rtrange, ncol = 2)
+        rtrange <- matrix(nrow = nrow(object@rtrange), ncol = 2)
+        rtrange[sampidx,] <- matrix(rep(t(tmprtrange), 
+                                        length = length(sampidx)*2), ncol = 2, 
+                                    byrow = TRUE)
+    }
+    
+    if (!missing(col) && length(col) < length(object@eic)) {
+        tmpcol <- col
+        col <- vector(class(col), length(object@eic))
+        col[sampidx] <- rep(tmpcol, length = length(sampidx))
+    }
+    
+    if (!missing(y)) {
+        xset <- y
+        pks <- peaks(xset)
+        pidx <- groupval(xset)
+        xsgrpidx <- match(object@groupnames, groupnames(xset, template = groupidx))
+        xssampidx <- match(names(object@eic), sampnames(xset))
+        
+        if (missing(col)) {
+            col <- unclass(sampclass(xset))[xssampidx]
+            if (length(palette()) < max(col))
+                col <- rainbow(max(col), end = 0.85)[col]
+        }
+        lcol <- col
+        for (i in seq(along = lcol)) {
+            rgbvec <- pmin(col2rgb(lcol[i])+153,255)
+            lcol[i] <- rgb(rgbvec[1], rgbvec[2], rgbvec[3], max = 255)
+        }
+        
+        if (missing(legtext))
+            legtext <- levels(sampclass(xset))
+    } else {
+        if (missing(col))
+            col <- rep(1, length(object@eic))
+    }
+    
+    for (i in grpidx) {
+        maxint <- numeric(length(sampidx))
+        for (j in seq(along = sampidx))
+            maxint[j] <- max(object@eic[[sampidx[j]]][[i]][,"intensity"])
+        plot(0, 0, type = "n", xlim = rtrange[i,], ylim = c(0, max(maxint)),
+             xlab = "Retention Time (seconds)", ylab = "Intensity",
+             main = paste("Extracted Ion Chromatogram:", object@mzrange[i,1], "-",
+                          object@mzrange[i,2], "m/z"))
+        for (j in sampidx[order(maxint, decreasing = TRUE)]) {
+            pts <- object@eic[[j]][[i]]
+            if (missing(y) || !peakint)
+                points(pts, type = "l", col = col[j])
+            else {
+                points(pts, type = "l", col = lcol[j])
+                peakrange <- pks[pidx[xsgrpidx[i],xssampidx[j]], c("rtmin","rtmax")]
+                if (object@rt == "raw") {
+                    corrt <- xset@rt$corrected[[xssampidx[j]]]
+                    cidx <- which(corrt >= peakrange[1] & corrt <= peakrange[2])
+                    peakrange <- xset@rt$raw[[xssampidx[j]]][c(cidx[1],cidx[length(cidx)])]
+                }
+                ptsidx <- pts[,"rt"] >= peakrange[1] & pts[,"rt"] <= peakrange[2]
+                points(pts[ptsidx,], type = "l", col = col[j])
+            }
+        }
+        
+        if (!is.null(legtext))
+            if (is.numeric(col))
+                legend(rtrange[i,2], max(maxint), legtext[unique(col[sampidx])], 
+                       col = unique(col[sampidx]), lty = 1, xjust = 1)
+            else  ### THIS NEEDS A BIT OF WORK ###
+                legend(rtrange[i,2], max(maxint), legtext, 
+                       col = unique(col[sampidx]), lty = 1, xjust = 1)
+    
+        
+        if (sleep > 0)
+            Sys.sleep(sleep)
+    }
+})
