c1ecc2369adbb6aacdb581a47086ed76a140bed1
colin <colin@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-12-07 18:09:37 +0000

    replaced clustuniqe with C implementation, rectUnique
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@15252 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/util.c
+++ src/util.c
@@ -146,6 +146,40 @@ void WhichRowMax(const double *in, const int *dn, const int *p, int *out) {
         out[i]++;
 }
 
+int ComparIntIndex(void *numptr, const void *i1, const void *i2) {
+
+    int *num = (int*)numptr;
+    
+    return num[*(int*)i1] - num[*(int*)i2];
+}
+
+void RectUnique(const double *m, const int *order, const int *nrow, 
+                const int *ncol, const double *xdiff, const double *ydiff,
+                int *keep) {
+
+    int    i, j, io, jo;
+    int    x1 = 0, x2 = *nrow, y1 = *nrow*2, y2 = *nrow*3;
+    
+    for (i = 0; i < *nrow; i++) {
+        io = order[i];
+        keep[io] = 1;
+        for (j = 0; j < i; j++) {
+            jo = order[j];
+            if (keep[jo] && 
+                !(m[x1+io] - m[x2+jo] > *xdiff || m[x1+jo] - m[x2+io] > *xdiff ||
+                  m[y1+io] - m[y2+jo] > *ydiff || m[y1+jo] - m[y2+io] > *ydiff)) {
+                keep[io] = 0;
+                /* Debuging Code
+                printf("%i\t%3.1f\t%3.1f\t%4.0f\t%4.0f\t%i\t%3.1f\t%3.1f\t%4.0f\t%4.0f\n",
+                       i, m[x1+io], m[x2+io], m[y1+io], m[y2+io],
+                       j, m[x1+jo], m[x2+jo], m[y1+jo], m[y2+jo]);
+                */
+                break;
+            }
+        }
+    }
+}
+
 SEXP DoubleMatrix(SEXP nrow, SEXP ncol) {
     
     SEXP matrix, dim;
