7a91a8dd1cdb3081217784b7dfa4296d272e84e6
Steffen Neumann <sneumann@ipb-halle.de>
2008-01-24 13:23:39 +0000

    Fix in plot(eic) for xcmsEIC with absent groups
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@29667 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xcmsEIC.R
+++ R/xcmsEIC.R
@@ -1,18 +1,18 @@
 setClass("xcmsEIC", representation(eic = "list", mzrange = "matrix",
-                                   rtrange = "matrix", rt = "character", 
+                                   rtrange = "matrix", rt = "character",
                                    groupnames = "character"),
-         prototype(eic = list(), mzrange = matrix(nrow = 0, ncol = 0), 
-                   rtrange = matrix(nrow = 0, ncol = 0), 
+         prototype(eic = list(), mzrange = matrix(nrow = 0, ncol = 0),
+                   rtrange = matrix(nrow = 0, ncol = 0),
                    rt = character(0), groupnames = character(0)))
 
 setMethod("show", "xcmsEIC", function(object) {
 
     cat("An \"xcmsEIC\" object with", length(object@eic), "samples\n\n")
-    
-    cat("Time range: ", paste(round(range(object@rtrange), 1), collapse = "-"), 
-        " seconds (", paste(round(range(object@rtrange)/60, 1), collapse = "-"), 
+
+    cat("Time range: ", paste(round(range(object@rtrange), 1), collapse = "-"),
+        " seconds (", paste(round(range(object@rtrange)/60, 1), collapse = "-"),
         " minutes)\n", sep = "")
-    cat("Mass range:", paste(round(range(object@mzrange, na.rm = TRUE), 4), collapse = "-"), 
+    cat("Mass range:", paste(round(range(object@mzrange, na.rm = TRUE), 4), collapse = "-"),
         "m/z\n")
     cat("EICs per sample:", nrow(object@mzrange), "\n")
     cat("Retention times:", object@rt, "\n")
@@ -22,8 +22,8 @@ setMethod("show", "xcmsEIC", function(object) {
     else
         cat("absent")
     cat("\n\n")
-    
-    
+
+
     memsize <- object.size(object)
     cat("Memory usage:", signif(memsize/2^20, 3), "MB\n")
 })
@@ -56,43 +56,47 @@ setMethod("rtrange", "xcmsEIC", function(object) {
     object@rtrange
 })
 
-plot.xcmsEIC <- function(x, y, groupidx = groupnames(x), sampleidx = sampnames(x), 
-                         rtrange = x@rtrange, col = rep(1, length(sampleidx)), legtext = NULL, 
+plot.xcmsEIC <- function(x, y, groupidx = groupnames(x), sampleidx = sampnames(x),
+                         rtrange = x@rtrange, col = rep(1, length(sampleidx)), legtext = NULL,
                          peakint = TRUE, sleep = 0, ...) {
 
     object <- x
-    
+
     if (is.numeric(groupidx))
        groupidx <- object@groupnames[groupidx]
-    grpidx <- match(groupidx, object@groupnames)
-    
+
+    if (length(object@groupnames))
+        grpidx <- match(groupidx, object@groupnames)
+    else
+        grpidx <- 1
+
     if (is.numeric(sampleidx))
        sampleidx <- names(object@eic)[sampleidx]
     sampidx <- match(sampleidx, names(object@eic))
-    
-    if (length(rtrange) == 1)
+
+    if (length(rtrange) == 1) {
         rtrange <- retexp(object@rtrange, rtrange)
-    else if (length(rtrange) < length(object@rtrange)) {
+    } else if (length(rtrange) < length(object@rtrange)) {
         tmprtrange <- matrix(rtrange, ncol = 2)
         rtrange <- matrix(nrow = nrow(object@rtrange), ncol = 2)
-        rtrange[sampidx,] <- matrix(rep(t(tmprtrange), 
-                                        length = length(sampidx)*2), ncol = 2, 
+        rtrange[sampidx,] <- matrix(rep(t(tmprtrange),
+                                        length = length(sampidx)*2), ncol = 2,
                                     byrow = TRUE)
     }
-    
+
     if (!missing(col) && length(col) < length(object@eic)) {
         tmpcol <- col
         col <- vector(class(col), length(object@eic))
         col[sampidx] <- rep(tmpcol, length = length(sampidx))
     }
-    
+
     if (!missing(y)) {
         xset <- y
         pks <- peaks(xset)
         pidx <- groupval(xset)
         xsgrpidx <- match(object@groupnames, groupnames(xset, template = groupidx))
         xssampidx <- match(names(object@eic), sampnames(xset))
-        
+
         if (missing(col)) {
             col <- unclass(sampclass(xset))[xssampidx]
             if (length(palette()) < max(col))
@@ -103,14 +107,14 @@ plot.xcmsEIC <- function(x, y, groupidx = groupnames(x), sampleidx = sampnames(x
             rgbvec <- pmin(col2rgb(lcol[i])+153,255)
             lcol[i] <- rgb(rgbvec[1], rgbvec[2], rgbvec[3], max = 255)
         }
-        
+
         if (missing(legtext))
             legtext <- levels(sampclass(xset))
     } else {
         if (missing(col))
             col <- rep(1, length(object@eic))
     }
-    
+
     for (i in grpidx) {
         maxint <- numeric(length(sampidx))
         for (j in seq(along = sampidx))
@@ -135,16 +139,16 @@ plot.xcmsEIC <- function(x, y, groupidx = groupnames(x), sampleidx = sampnames(x
                 points(pts[ptsidx,], type = "l", col = col[j])
             }
         }
-        
+
         if (!is.null(legtext))
             if (is.numeric(col))
-                legend(rtrange[i,2], max(maxint), legtext[unique(col[sampidx])], 
+                legend(rtrange[i,2], max(maxint), legtext[unique(col[sampidx])],
                        col = unique(col[sampidx]), lty = 1, xjust = 1)
             else  ### THIS NEEDS A BIT OF WORK ###
-                legend(rtrange[i,2], max(maxint), legtext, 
+                legend(rtrange[i,2], max(maxint), legtext,
                        col = unique(col[sampidx]), lty = 1, xjust = 1)
-    
-        
+
+
         if (sleep > 0)
             Sys.sleep(sleep)
     }
