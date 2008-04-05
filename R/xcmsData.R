608bd1d730546f1b5331de9cb9f7c8c9329f845a
Michael Lawrence <lawremi@iastate.edu>
2008-04-05 21:06:15 +0000

    Fix a few bugs in the pipeline framework. Add profileMatrix<- for setting a pre-generated profile matrix on an xcmsRaw.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@30869 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xcmsData.R
+++ R/xcmsData.R
@@ -4,7 +4,7 @@ setMethod("pipeline", "xcmsData", function(object, local = FALSE)
           {
             pipeline <- object@pipeline
             if (local) {
-              me <- sapply(sapply(pipeline, outType), is, class(object))
+              me <- sapply(sapply(pipeline, outType), extends, class(object))
               if (any(!me)) {
                 pipeline <- object@pipeline
                 pipeline@.Data <- tail(pipeline, -tail(which(!me),1))
@@ -33,5 +33,5 @@ setMethod("explore", c("xcmsData", "missing"), function(object, protocol, ...)
             proto <- NULL
             if (length(object@pipeline@.Data))
               proto <- tail(object@pipeline,1)[[1]]
-            explore(proto, object)
+            explore(object, proto)
           })
