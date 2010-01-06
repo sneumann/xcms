aa58e05d9565dbc97931b7662cfeaa86c47dadd6
Steffen Neumann <sneumann@ipb-halle.de>
2010-01-06 23:22:54 +0000

    Fixed some warnings
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@43882 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/specDist.R
+++ R/specDist.R
@@ -17,39 +17,6 @@ setMethod("specDist", signature(object="xcmsSet"), function(object,peakIDs1,peak
     distance
     })
 
-setMethod("specDist", signature(object="xsAnnotate"), function(object,object2=NA, peakIDs1=NA,peakIDs2=NA,PSpec1=NA,PSpec2=NA, method=getOption("BioC")$xcms$specDist.method, 
-                                                               mzabs=0.001, mzppm=10,symmetric=FALSE,...) {
-    if (is.na(peakIDs1[1])&is.na(peakIDs2[1])&is.na(PSpec1)) stop("No index given. You must define at least either two vectors of peak-IDs (peakIDs1, peakIDs2) or the indicies for two Pseudospectra (PSpec1, PSpec2 )!")
-    if (class(object2)!="xsAnnotate") {
-        object2<-object
-        }
-    peaks1 <- object@peaks
-    peaks2 <- object2@peaks
-    method <- match.arg(method, getOption("BioC")$xcms$specDist.methods)
-        if (is.na(method))
-            stop("unknown method : ", method)
-        method <- paste("specDist", method, sep=".")
-
-    if (!is.na(peakIDs1[1]) & !is.na(peakIDs2[1])){
-        peakTable1 <- peaks1[peakIDs1,c("mz","into")]
-        peakTable2 <- peaks2[peakIDs2,c("mz","into")]
-        distance <- do.call(method, alist<-list(peakTable1, peakTable2, ...))
-        return(distance)
-    }else{
-        if (!is.na(PSpec1)&!is.na(PSpec2)) {
-            if (length(object@pspectra[[PSpec1]])>1) { peakTable1 <- cbind(object@pspectra[[PSpec1]], object@peaks[object@pspectra[[PSpec1]],])
-                }else{ peakTable1 <- cbind(object@pspectra[[PSpec1]], t(as.matrix(object@peaks[object@pspectra[[PSpec1]],])))}
-            colnames(peakTable1) <- c("oPeak",colnames(object@peaks))
-
-            if (length(object2@pspectra[[PSpec2]])>1) { peakTable2 <- cbind(object2@pspectra[[PSpec2]], object2@peaks[object2@pspectra[[PSpec2]],])
-                }else{ peakTable2 <- cbind(object2@pspectra[[PSpec2]], t(as.matrix(object2@peaks[object2@pspectra[[PSpec2]],])))}
-            colnames(peakTable2) <- c("oPeak",colnames(object2@peaks))    
-            distance <- do.call(method, alist<-list(peakTable1, peakTable2, mzabs=mzabs, mzppm=mzppm,...))
-            return(distance)
-            }
-        }
-    })
-
 ### specDist - functions
 setGeneric("specDist.meanMZmatch", function(peakTable1, peakTable2, ...) standardGeneric("specDist.meanMZmatch"))
 setMethod("specDist.meanMZmatch", signature(peakTable1="matrix", peakTable2="matrix"), function(peakTable1, peakTable2, matchdist=1, matchrate=1, mzabs=0.001, mzppm=10,symmetric=TRUE,...){
@@ -93,9 +60,16 @@ minifm <- function(v1, v2, mzabs,mzppm,symmetric)
     list(idx1=idx1, idx2=idx2)
     }
 
-setGeneric("specDist.cosinus", function(peakTable1, peakTable2, ...) standardGeneric("specDist.cosinus"))
-setMethod("specDist.cosinus", signature(peakTable1="matrix", peakTable2="matrix"), function(peakTable1, peakTable2, mzExp=0.6, intExp=3, nPdiff=2, 
-                                                                                            nPmin=8, mzabs=0.001, mzppm=10,symmetric=FALSE,...){
+setGeneric("specDist.cosinus", function(peakTable1, peakTable2,
+                                        mzabs = 0.001, mzppm = 10,
+                                        mzExp = 0.6, intExp = 3,
+                                        nPdiff = 2, nPmin = 8,
+                                        symmetric = FALSE)
+           standardGeneric("specDist.cosinus"))
+setMethod("specDist.cosinus", signature(peakTable1="matrix", peakTable2="matrix"), function(peakTable1, peakTable2, mzabs = 0.001, mzppm = 10,
+                                        mzExp = 0.6, intExp = 3,
+                                        nPdiff = 2, nPmin = 8,
+                                        symmetric = FALSE){
     len1 <- nrow(peakTable1)
     len2 <- nrow(peakTable2)
     if ((len1<len2 & len2/len1 > nPdiff)|(len1>len2 & len1/len2 > nPdiff)) {
@@ -141,8 +115,8 @@ setMethod("specDist.cosinus", signature(peakTable1="matrix", peakTable2="matrix"
      })
 
 
-setGeneric("specDist.peakCount", function(peakTable1, peakTable2, ...) standardGeneric("specDist.peakCount"))
-setMethod("specDist.peakCount", signature(peakTable1="matrix", peakTable2="matrix"), function(peakTable1, peakTable2, mzabs=0.001, mzppm=10,symmetric=FALSE,...){
+setGeneric("specDist.peakCount", function(peakTable1, peakTable2, mzabs=0.001, mzppm=10,symmetric=FALSE) standardGeneric("specDist.peakCount"))
+setMethod("specDist.peakCount", signature(peakTable1="matrix", peakTable2="matrix"), function(peakTable1, peakTable2, mzabs=0.001, mzppm=10,symmetric=FALSE){
     idx<-minifm(peakTable1[,"mz"],peakTable2[,"mz"],mzabs=mzabs,mzppm=mzppm,symmetric)
     return(length(idx$idx1))
         })
