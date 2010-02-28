a0be357f0f32dd88d6b1acd48d70480c92b38dcc
Steffen Neumann <sneumann@ipb-halle.de>
2010-02-28 22:31:45 +0000

    fixed handling of sampclasses / complex phenoData
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@44935 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.phenoData.R
+++ inst/unitTests/runit.phenoData.R
@@ -8,9 +8,13 @@ testMultiFactor <- function() {
              "./E261/const/WT/wt18.CDF", "./E261/const/WT/wt19.CDF",
              "./E261/const/WT/wt21.CDF", "./E261/const/WT/wt22.CDF")
   pd <- xcms:::phenoDataFromPaths(files)
+  xs <- faahko
   
-  phenoData(faahko) <- pd
-  xsg <- group(faahko)
+  ##xcms::phenoData(xs) <- pd
+  ## https://stat.ethz.ch/pipermail/r-devel/2008-April/049184.html
+  xs <- xcms::`phenoData<-`(xs, pd)
+
+  xsg <- group(xs)
 }
 
 testMultiFactorDiffreport <- function() {
@@ -23,8 +27,12 @@ testMultiFactorDiffreport <- function() {
              "./E261/const/WT/wt18.CDF", "./E261/const/WT/wt19.CDF",
              "./E261/const/WT/wt21.CDF", "./E261/const/WT/wt22.CDF")
   pd <- xcms:::phenoDataFromPaths(files)
+  xs <- faahko
   
-  phenoData(faahko) <- pd
-  xs <- fillPeaks(group(faahko))
-  dr <- diffreport(xsgf, class1="E260.KO", class2="E261.WT")
+  ##xcms::phenoData(xs) <- pd
+  ## https://stat.ethz.ch/pipermail/r-devel/2008-April/049184.html
+  xs <- xcms::`phenoData<-`(xs, pd)
+
+  xs <- fillPeaks(group(xs))
+  dr <- diffreport(xs, class1="E260.KO", class2="E261.WT")
 }
