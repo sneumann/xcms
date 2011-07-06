a655cd3a8095d282520b42de410b41e2792f0fdf
Paul Benton <hpbenton@scripps.edu>
2011-07-06 15:25:45 +0000

    Removed xcms2 functionality Increased speed of stitch function.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@56586 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/CleanData.R
+++ R/CleanData.R
@@ -17,92 +17,70 @@ setMethod("makeacqNum", "xcmsRaw", function(object, freq, start=1) {
 setGeneric("stitch", function(object, lockMass) standardGeneric("stitch"))
 
 setMethod("stitch", "xcmsRaw", function(object, lockMass) {
-		require(stats)
-		if(file.exists("mz.txt")){
-			unlink("mz.txt")
+	ob<-new("xcmsRaw")
+	ob@env$mz<-object@env$mz
+	ob@env$intensity<-object@env$intensity
+	ob@scanindex<-object@scanindex
+	ob@scantime<-object@scantime
+
+	ob@acquisitionNum<-1:length(ob@scanindex)
+	ob@filepath<-object@filepath
+	ob@mzrange<-range(ob@env$mz)
+	ob@profmethod<-object@profmethod
+	ob@tic<-object@tic
+	ob@profparam<-list()
+
+	arr<-array(dim=c(2,max(diff(ob@scanindex)), length(ob@scanindex)))
+	if(lockMass[1] == 1){
+		lockMass<-lockMass[3:length(lockMass)]
+	}
+	lockMass<-matrix(lockMass, ncol=2, byrow=TRUE)
+
+	for(i in 1:(length(ob@scanindex)-1)){
+		if(any(i == lockMass[,1])){
+			arr[1,,i] <-c(object@env$mz[(object@scanindex[(i-1)]+1):object@scanindex[i]], 
+				rep(NA, (max(diff(object@scanindex))-length((object@scanindex[(i-1)]+1):object@scanindex[i])) ))
+
+			arr[2,,i] <-c(object@env$intensity[(object@scanindex[(i-1)]+1):object@scanindex[i]], 
+					rep(NA, (max(diff(object@scanindex)) - 
+					length((object@scanindex[(i-1)]+1):object@scanindex[i])) ))
+
+		} else if(any(i == lockMass[,2])){
+			arr[1,,i] <-c(object@env$mz[(object@scanindex[i+1]+1):object@scanindex[(i+2)]], 
+				rep(NA, (max(diff(object@scanindex)) - 
+				length((object@scanindex[i+1]+1):object@scanindex[(i+2)])) ))
+
+			arr[2,,i] <-c(object@env$intensity[(object@scanindex[i+1]+1):object@scanindex[(i+2)]], 
+					rep(NA, (max(diff(object@scanindex)) - 
+					length((object@scanindex[i+1]+1):object@scanindex[(i+2)])) ))
+
+		} else{
+			arr[1,,i] <-c(object@env$mz[(object@scanindex[i]+1):object@scanindex[i+1]], 
+				rep(NA, (max(diff(object@scanindex))-length((object@scanindex[i]+1):object@scanindex[i+1])) ))
+
+			arr[2,,i] <-c(object@env$intensity[(object@scanindex[i]+1):object@scanindex[i+1]], 
+				rep(NA, (max(diff(object@scanindex)) - 
+				length((object@scanindex[i]+1):object@scanindex[i+1])) ))
 		}
-		if(file.exists("intensity.txt")){
-			unlink("intensity.txt")
-		}
+		## mz is in 1; Intensity is in 2
+		##remake scanindex
+		if(i == 1){
+			ob@scanindex[i]<-as.integer(0)
 
-		hold<-0
-		opps<-0
-		scanindex<-list()
-
-		for(i in 1:length(object@acquisitionNum)){
-			percent<-(i/length(object@acquisitionNum))*100
-			#cat(paste(round(percent), "%\r"))
-			if( i == 1 || i == 2 ){ 
-				scan<-getScan(object, i)
-				cat(scan[,"mz"], " ", append=TRUE, file="mz.txt")
-				cat(scan[,"intensity"], " ", append=TRUE, file="intensity.txt")
-				next # since lock mass is 1 & 2 there is no gap to fill :)
-			}else if(i == (hold+1)){ 
-				next 
-			}else if(all(lockMass != i ) || i >= (length(object@acquisitionNum) - 2) ){
-				#Get all the other scans into the text file as well
-				scan<-getScan(object, i)
-				cat(scan[,"mz"], " ", append=TRUE, file="mz.txt")
-				cat(scan[,"intensity"], " ", append=TRUE, file="intensity.txt")
-				next
-			}else if(any(lockMass == i)){
-				hold<-i
-				cat(paste(basename(object@filepath), " :\t", round(percent), "%\r"))
-
-				scan<-getScan(object, i-1)
-				scanB<-getScan(object, i+2)
-				cat(scan[,"intensity"], " ", scanB[,"intensity"], " ", append=TRUE, file="intensity.txt")
-				cat(scan[,"mz"], " ", scanB[,"mz"], " ",append=TRUE, file="mz.txt")
-				##Fill the gap with the scan before and jsut after lock Mass scans 
-			}
-		}
-		cat("\n")
-
-		##Now all loops are finished we can add the mz.txt and Intensity.txt 
-		##to the object and make the scanindex :)
-		gc()
-		if(file.exists("mz.txt") && file.exists("intensity.txt")){
-			#cat(paste("Reading m/z & intensity Vectors...\n", sep=""))
-			mz<-scan("mz.txt", what="numeric")
-			intensity<-scan("intensity.txt", what="numeric")
+		}else if(i == length(ob@scanindex)-1){
+			ob@scanindex[i]<-as.integer(length(na.omit(arr[1,,(i-1)]))+ob@scanindex[(i-1)])
+			ob@scanindex[i+1]<-as.integer(length(na.omit(arr[1,,i]))+ob@scanindex[i])
+#			ob@scanindex[i+1]<-as.integer(length(ob@env$mz))
 		}else{
-			stop("ERROR: Hard Drive error - m/z and Intensity Vectors not found\n")
+			ob@scanindex[i]<-as.integer(length(na.omit(arr[1,,(i-1)]))+ob@scanindex[(i-1)])
 		}
-		mz<-as.numeric(mz)
-		intensity<-as.numeric(intensity)
+	}
 
-		scanIdx<-0
-		for(k in 1:length(mz)){
-			if( (k+2) == (length(mz))){
-				break
-			} else if(mz[k+1] > mz[k]){
-				#cat(k, " <-k \n")
-				if(mz[k+2] < mz[k+1] && mz[k] < mz[k+2]){
-					stop("Error at ", k, " : value greater than element k\n")
-				}else{
-					next
-				}
-			}else if(mz[k+1] < mz[k]){
-				scanIdx<-c(scanIdx, k)
-	#			cat("N--> ",n," <--N \n")
-			}
-		}
-		
-		ob<-new("xcmsRaw")
-		ob@scanindex<-as.integer(scanIdx)
-		ob@env$mz<-mz
-		ob@env$intensity<-intensity
-		ob@acquisitionNum<-1:length(scanIdx)
-		ob@filepath<-object@filepath
-		ob@mzrange<-range(mz)
-		ob@profmethod<-object@profmethod
-		ob@tic<-object@tic
-		ob@scantime<-object@scantime
-		ob@profparam<-list()
-		
-		rm(object,scanIdx, mz, intensity)
-		gc()
-		cat("\n")
-		return(ob)
+	NAidx<-is.na(arr[1,,])
+	ob@env$mz<-as.numeric(arr[1,,][!NAidx])
+	ob@env$intensity<-as.numeric(arr[2,,][!NAidx])
+
+	return(ob)
 })
+
 
