9f8498a29c203132608ac9742a9277e489cdffef
Ralf Tautenhahn <rtautenh@ipb-halle.de>
2013-03-26 21:24:10 +0000

    added unit test to catch indexing bug in group.nearest
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@74868 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.group.R
+++ inst/unitTests/runit.group.R
@@ -11,3 +11,11 @@ testGroupDensityOnePeak <- function() {
 ##   peaks(xs) <- p[1,,drop=FALSE]
 ##   g <- group(xs, method="nearest")
 ## }
+
+testGroupNearest<- function() {
+  xs <- faahko
+  p <- peaks(xs)
+  g <- group(xs, method="nearest")
+  checkEquals(range(unlist(g@groupidx))[1],  1)
+}
+
