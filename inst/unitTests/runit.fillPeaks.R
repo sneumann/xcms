c65840422356ca88f93d609097ba164c5338b26e
Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
2016-01-16 15:28:35 -0800

    Commit made by the Bioconductor Git-SVN bridge.
    Consists of 1 commit.
    
    Commit information:
    
    SVN Revision number: 112627
    Commit message:
    Move to ncdf4, disable parallel unit testing
    Committed by s.neumann
    Committed at: 2016-01-16T23:09:31.477674Z

--- inst/unitTests/runit.fillPeaks.R
+++ inst/unitTests/runit.fillPeaks.R
@@ -9,7 +9,7 @@ testFillPeaksPar <- function() {
     xsg <- group(faahko)
 
     xsgfSerial <- fillPeaks(xsg, method="chrom")
-    xsgfParallel <- fillPeaks(xsg, method="chrom", nSlaves=2)
+    xsgfParallel <- fillPeaks(xsg, method="chrom") # parallel disabled: , nSlaves=2)
 
     checkEqualsNumeric(nrow(peaks(xsgfSerial)),
                        nrow(peaks(xsgfParallel)))
@@ -22,23 +22,23 @@ test.fillPeaksColumns <- function() {
   peaks(xsg) <- cbind(peaks(xsg), anotherColumn=4711)
 
   oldCnames <- colnames(peaks(xsg))
-  xsgf <- fillPeaks(xsg, nSlaves=2)
+  xsgf <- fillPeaks(xsg) # parallel disabled: , nSlaves=2)
 
   newCnames <- colnames(peaks(xsgf))
   checkEquals(oldCnames, newCnames)
 
   ## Check dims if nothing to do
   oldDims <- dim(peaks(xsgf))
-  xsgf2 <- fillPeaks(xsgf, nSlaves=2)
+  xsgf2 <- fillPeaks(xsgf) # parallel disabled: , nSlaves=2)
   newDims <- dim(peaks(xsgf2))
   checkEquals(oldDims, newDims)
 
   ## Case where only some samples have NA values
   xsg <- group(faahko, minfrac=1)
-  xsgf <- fillPeaks(xsg, nSlaves=2)
+  xsgf <- fillPeaks(xsg) # parallel disabled: , nSlaves=2)
   sampclass(xsgf) <- c(rep("KO", 5), rep("WT", 7))
   xsgf <- group(xsgf, minfrac=1)
-  xsgf <- fillPeaks(xsgf, nSlaves=2)
+  xsgf <- fillPeaks(xsgf) # parallel disabled: , nSlaves=2)
   
 }
 
