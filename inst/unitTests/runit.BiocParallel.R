e81b590fd2596829103798e5d8b19e2fdfde2018
jotsetung <johannes.rainer@gmail.com>
2016-07-18 14:41:57 +0200

    Use BiocParallel for parallel processing
    o Use bplapply in getXcmsRaw.
    o Deprecate xcmsPapply.
    o Update documentations to include BPPARAM argument.
    o Import from BiocGenerics and Biobase, remove the definitions of
      phenoData and phenoData<- generics.
    o Import from graphics, stats etc to fix 'R CMD check' warnings.

--- inst/unitTests/runit.BiocParallel.R
+++ inst/unitTests/runit.BiocParallel.R
@@ -10,35 +10,10 @@ test_BiocParallel <- function() {
             system.file('cdf/KO/ko18.CDF', package = "faahKO"),
             system.file('cdf/KO/ko19.CDF', package = "faahKO"))
 
-    xs <- xcmsSet(fs, nSlaves = 2)
-
     ## Default.
     library(BiocParallel)
     bpp <- bpparam()
     bpprogressbar(bpp) <- TRUE
-    xs2 <- xcms:::xcmsSet2(fs, BPPARAM=bpp)
-    checkIdentical(xs, xs2)
-
-    bpp <- SerialParam()
-    bpprogressbar(bpp) <- TRUE
-    xs2 <- xcms:::xcmsSet2(fs, BPPARAM=bpp)
-    checkIdentical(xs, xs2)
-
-    bpp <- MulticoreParam()
-    bpprogressbar(bpp) <- TRUE
-    xs2 <- xcms:::xcmsSet2(fs, BPPARAM=bpp)
-    checkIdentical(xs, xs2)
-
-    ## With fill peaks.
-    xs <- group(xs)
-    xsf <- fillPeaks.chrom(xs, nSlaves = 2)
-    xsf2 <- xcms:::fillPeaks.chrom2(xs)
-    checkIdentical(xsf, xsf2)
-
-    ## Serial processing
-    bpp <- SerialParam()
-    bpprogressbar(bpp) <- TRUE
-    xsf2 <- xcms:::fillPeaks.chrom2(xs, BPPARAM=bpp)
-    checkIdentical(xsf, xsf2)
+    xs2 <- xcmsSet(fs, BPPARAM=bpp)
 
 }
