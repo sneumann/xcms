a989bbdc350bd34b3a2606e8b982ce0bf99d2b16
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-09-05 11:13:01 +0000

    Added function findPeaks.centWaveWithPredictedIsotopeROIs for the sensitive detection of isotope peaks based on predicted isotope ROIs
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/xcms@120721 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/runit.findPeaksCentWaveWithIsotopeROIs.R
@@ -0,0 +1,25 @@
+
+test.findPeaksCentWaveWithIsotopeROIs <- function() {
+    file <- system.file('cdf/KO/ko15.CDF', package = "faahKO")
+    xr <- xcmsRaw(file)
+    p1 <- findPeaks.centWave(xr)
+    
+    addROIsParams <- list()
+    addROIsParams$addNewROIs <- TRUE
+    addROIsParams$addNewIsotopeROIs <- TRUE
+    addROIsParams$addNewAdductROIs  <- FALSE
+    addROIsParams$snthreshOfGeneratedROIs <- 6.25
+    addROIsParams$maxcharge  <- 3
+    addROIsParams$maxiso     <- 5
+    addROIsParams$mzIntervalExtension <- TRUE
+    
+    p2 <- findPeaks.centWaveWithPredictedIsotopeROIs(
+      xr, 
+      ROI.list=list(), 
+      addROIsParams = addROIsParams
+    )
+    
+    checkTrue(nrow(p1)>nrow(p2))
+    
+}
+
