55ad93aea1db35872ad30ca969fea975000b80e8
jotsetung <johannes.rainer@gmail.com>
2018-03-05 10:08:40 +0100

    Fix issue #262
    - Add robustbase to enhances and check that, if required, the package is
      installed.

--- R/functions-normalization.R
+++ R/functions-normalization.R
@@ -46,7 +46,7 @@ fitModel <- function(formula, data, y, minVals = 4,
                      method = c("lm", "lmrob"), BPPARAM = bpparam()) {
     method <- match.arg(method, c("lm", "lmrob"))
     if (method == "lmrob") {
-        if (!require("robustbase", character.only = TRUE, quietly = TRUE))
+        if (!requireNamespace("robustbase", quietly = TRUE))
             stop("Required package 'robustbase' is not installed. Please ",
                  "install if you want to use method 'lmrob'.")
     }
@@ -87,7 +87,7 @@ fitModel <- function(formula, data, y, minVals = 4,
     if (method == "lmrob") {
         ## Force use of the KS2014 settings in lmrob and increase the
         ## scale-finding iterations to avoid some of the warnings.
-        sttngs <- lmrob.control("KS2014")
+        sttngs <- robustbase::lmrob.control("KS2014")
         sttngs$maxit.scale <- 10000
         sttngs$k.max <- 10000
         sttngs$refine.tol <- 1e-7
@@ -109,7 +109,7 @@ fitModel <- function(formula, data, y, minVals = 4,
                 return(lm(formula., data = data., model = FALSE))
             if (lmeth == "lmrob") {
                 set.seed(123)
-                return(lmrob(formula., data = data., model = FALSE,
+                return(robustbase::lmrob(formula., data = data., model = FALSE,
                                          control = sttngs))
             }
             if (lmeth == "rlm")
--- inst/unitTests/runit.functions-normalization.R
+++ inst/unitTests/runit.functions-normalization.R
@@ -1,6 +1,6 @@
 ## Unit tests for functions in functions-normalization.R
 
-test.fitModel <- function() {
+test_fitModel <- function() {
     vals <- featureValues(xod_xgrg)
     dat <- data.frame(injection_idx = 1:length(fileNames(xod_xgrg)))
     fits <- xcms:::fitModel(formula = y ~ injection_idx, y = vals, minVals = 3,
@@ -8,4 +8,9 @@ test.fitModel <- function() {
     ## Check that we've got NULL for features with less thanb 3 values.
     nulls <- apply(vals, MARGIN = 1, function(z) any(is.na(z)))
     checkEquals(nulls, lengths(fits) == 0)
+
+    ## Check that robustbase would work
+    if (requireNamespace("robustbase", quietly = TRUE))
+        fits <- xcms:::fitModel(formula = y ~ injection_idx, y = vals,
+                                minVals = 3, data = dat, method = "lmrob")
 }
