8569b174c8054f40ecc5c7ab41afddf8750f8ca7
jotsetung <johannes.rainer@gmail.com>
2018-11-22 15:18:53 +0100

    Add chromPeaks,XChromatogram method (issue #329).

--- R/methods-XChromatogram.R
+++ R/methods-XChromatogram.R
@@ -7,3 +7,65 @@ setMethod("show", "XChromatogram", function(object) {
         cat(" ", paste(object@chromPeaks[i, ], collapse = "\t"), sep = "")
     cat("\n")
 })
+
+#' @rdname XChromatogram
+#'
+#' @description
+#'
+#' - `chromPeaks`, `chromPeaks<-`: extract or set the matrix with the
+#'   chromatographic peak definitions. Parameter `rt` allows to specify a
+#'   retention time range for which peaks should be returned along with
+#'   parameter `type` that defines how *overlapping* is defined (parameter
+#'   description for details).
+#'
+#' @param rt For `chromPeaks`: `numeric(2)` defining the retention time range
+#'     for which chromatographic peaks should be returned.
+#'
+#' @param type For `chromPeaks`: `character(1)` defining which peaks to return
+#'     if `rt` is provided: `"any"` (default) return all peaks that are even
+#'     partially overlapping with `rt`, `"within"` return peaks that are
+#'     completely within `rt` and `"apex_within"` return peaks which apex
+#'     is within `rt`.
+#'
+#' @param value For `chromPeaks<-`: a numeric `matrix` with required columns
+#'     `"rt"`, `"rtmin"`, `"rtmax"`, `"into"` and `"maxo"`.
+#'
+#' @return
+#'
+#' For `chromPeaks`: a `matrix` with columns:
+#' - `"rt"`: the retention time of the peak apex.
+#' - `"rtmin"`: the lower peak boundary.
+#' - `"rtmax"`: the upper peak boundary.
+#' - `"into"`: the integrated peak signal (area of the peak).
+#' - `"maxo"`: the maximum intensity of the peak.
+#'
+#' ## Extract the chromatographic peaks
+#' chromPeaks(xchr)
+setMethod("chromPeaks", "XChromatogram", function(object, rt = numeric(),
+                                                  type = c("any", "within",
+                                                           "apex_within")) {
+    type <- match.arg(type)
+    pks <- object@chromPeaks
+    if (length(rt) && nrow(pks)) {
+        rt <- range(rt)
+        pks <- switch(type,
+                      any = pks[which(pks[, "rtmin"] <= rt[2] &
+                                      pks[, "rtmax"] >= rt[1]), , drop = FALSE],
+                      within = pks[which(pks[, "rtmin"] >= rt[1] &
+                                         pks[, "rtmax"] <= rt[2]), ,
+                                   drop = FALSE],
+                      apex_within = pks[which(pks[, "rt"] >= rt[1] &
+                                              pks[, "rt"] <= rt[2]), ,
+                                        drop = FALSE]
+                      )
+    }
+    pks
+})
+
+#' @rdname XChromatogram
+setReplaceMethod("chromPeaks", "XChromatogram", function(object, value) {
+    if (!is.matrix(value))
+        stop("'value' should be a numeric matrix")
+    object@chromPeaks <- value
+    if (validObject(object)) object
+})
--- tests/testthat/test_methods-XChromatogram.R
+++ tests/testthat/test_methods-XChromatogram.R
@@ -1,3 +1,26 @@
 test_that("show,XChromatogram works", {
     show(XChromatogram())
 })
+
+test_that("chromPeaks,chromPeaks<-,XChromatogram works", {
+    chr <- Chromatogram(rtime = 1:10,
+                        intensity = c(4, 12, 18, 24, 23, 18, 15, 3, 2, 5))
+    xchr <- XChromatogram(chr)
+    expect_true(nrow(chromPeaks(xchr)) == 0)
+    pks <- matrix(nrow = 4, ncol = 6)
+    colnames(pks) <- xcms:::.CHROMPEAKS_REQ_NAMES
+    pks[1, ] <- c(4, 2, 8, 24, NA, NA)
+    pks[2, ] <- c(3, 2, 7, 24, NA, NA)
+    pks[3, ] <- c(9, 7, 10, 2, NA, NA)
+    pks[4, ] <- c(8, 5, 10, 3, NA, NA)
+    expect_error(chromPeaks(xchr) <- 4)
+    chromPeaks(xchr) <- pks
+    expect_equal(chromPeaks(xchr), pks)
+
+    expect_true(nrow(chromPeaks(xchr, rt = c(20, 30))) == 0)
+    expect_equal(chromPeaks(xchr, rt = c(2, 7)), pks)
+    expect_equal(chromPeaks(xchr, rt = c(2, 7), type = "apex_within"),
+                 pks[1:2, ])
+    expect_equal(chromPeaks(xchr, rt = c(2, 7), type = "within"),
+                 pks[2, , drop = FALSE])
+})
