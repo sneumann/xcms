f3fc83c26b8b9670cfa9b9b118f652ce7e611f7a
wkumler <wkumler@uw.edu>
2020-02-10 13:32:28 -0800

    Making test case robust by adding numeric peak directly rather than trusting RNG

--- tests/testthat/test_do_findChromPeaks-functions.R
+++ tests/testthat/test_do_findChromPeaks-functions.R
@@ -259,12 +259,17 @@ test_that("peaksWithCentWave works", {
     pks <- peaksWithCentWave(intensity(chr), rtime(chr), fitgauss = TRUE)
     
     #Testing for Github issue #445 after introducing new CWP method
-    skinny_peak <- round((c(numeric(20), dnorm(seq(-3, 3, length.out = 20)), 
-                     numeric(20))*100 +runif(60))*10000)
+    # Values from round((c(numeric(20), dnorm(seq(-3, 3, length.out = 20)), 
+    #             numeric(20))*100 +runif(60))*10000)
+    skinny_peak <- c(9107, 3326, 9523, 3245, 3429, 9394, 1123, 935, 5128, 8576, 
+                     2711, 3427, 7294, 8109, 9288, 6997, 9756, 8034, 1317, 8866, 13877, 
+                     14854, 28296, 57101, 92209, 151797, 222386, 299402, 365045, 394255, 
+                     402680, 363996, 293985, 222989, 147007, 94947, 52924, 32438, 
+                     11511, 10836, 8046, 601, 889, 5917, 2690, 5381, 9901, 8494, 3349, 
+                     8283, 3410, 5935, 3332, 7041, 3284, 7478, 76, 3739, 2158, 5507)
     skinny_peak_rt <- seq_along(skinny_peak)+100
     pks <- peaksWithCentWave(skinny_peak, rt=skinny_peak_rt, snthresh = 0)
-    expect_true(nrow(pks==1))
-    
+    expect_true(nrow(pks==1))    
 
     ## Check errors
     expect_error(peaksWithCentWave())
