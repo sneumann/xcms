6dd26b77d5cde28ecd8484d47b60fe83c3e11f5d
wkumler <wkumler@uw.edu>
2023-07-31 17:33:01 -0700

    Actually adding verboseBetaColumns to documentation

--- R/DataClasses.R
+++ R/DataClasses.R
@@ -441,6 +441,14 @@ setClass("XProcessHistory",
 #' method to extend the EIC to a integer base-2 length prior to being passed to
 #' \code{convolve} rather than the default "reflect" method. See
 #' https://github.com/sneumann/xcms/issues/445 for more information.
+#' 
+#' @param verboseBetaColumns Option to calculate two additional metrics of peak
+#' quality via comparison to an idealized bell curve. Adds \code{beta_cor} and
+#' \code{beta_snr} to the \code{chromPeaks} output, corresponding to a Pearson
+#' correlation coefficient to a bell curve with several degrees of skew as well
+#' as an estimate of signal-to-noise using the residuals from the best-fitting
+#' bell curve. See https://github.com/sneumann/xcms/pull/685 for more 
+#' information.
 #'
 #' @details
 #'
